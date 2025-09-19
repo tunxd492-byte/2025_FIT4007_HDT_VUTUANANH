1020c8a80662df5290dc5aff32f9dd14145bb5bc
tunxd492-byte <tunxd492@gmail.com>
2025-09-19 22:40:45 +0700

    Update Code_Account.java

--- Code_Account.java
+++ Code_Account.java
@@ -1,1 +1,23 @@
+class Account {
+  private String MaSTK;
+  private double SoTien;
 
+  public Account(String MaSTK,double SoTien){
+    this.MaSTK=MaSTK;
+    this.SoTien=SoTien;
+  }
+  public String getMaSTK(){
+    return MaSTK;
+  }
+  public double getSoTien(){
+    return SoTien;
+  }
+  public void setSoTien(double SoTien){
+    this.SoTien=SoTien;
+  }
+  public void HienThi(){
+    System.out.println("Thông tin tài khoản: ");
+    System.out.println("Mã số tài khoản: " + MaSTK);
+    System.out.println("Số tiền: " + SoTien);
+  }
+}
